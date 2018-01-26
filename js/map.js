69a505146ec7815ea0f94fc6a44b3e439e5c3b12
MAURICIO RUANOVA HURTADO <mau@MAURICIOs-MacBook-Air.local>
2018-01-26 11:51:41 -0600

    first commit

--- js/map.js
+++ js/map.js
@@ -1,21 +1,3 @@
-var modal = document.getElementById('myModal');
-var btn = document.getElementById("about");
-var span = document.getElementsByClassName("close")[0];
-
-btn.onclick = function () {
-    modal.style.display = "block";
-};
-
-span.onclick = function () {
-    modal.style.display = "none";
-};
-
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-};
-
 var geocoder;
 var map;
 var markers = [];
